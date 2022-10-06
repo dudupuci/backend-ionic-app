9de73ca00f174d5aaad1ccd52380e46deed51158
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-10-06 00:30:07 -0300

    project created

---
+++ src/test/java/com/megashop/app/BackendIonicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megashop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIonicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
